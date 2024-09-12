166d6e2d394207d2ae60030045044825d46f368d
Henrique Yuji Rossetti Inonhe <henriqueinonhe@gmail.com>
2024-09-11 21:17:24 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Email Tracking do Amorzinho</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+    </>
+  );
+}
